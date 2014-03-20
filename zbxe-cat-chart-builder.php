45168e57146d60384c42668e88a98e8c48644e47
spaww <the.spaww@gmail.com>
2014-03-20 09:19:25 -0300

    2.0.1 - Capacity and Trends Chart

--- zbxe-cat-chart-builder.php
+++ zbxe-cat-chart-builder.php
@@ -1,3 +1,30 @@
+<?php
+
+/* Used for inicial development: 
+** Objective: Show a chart from data in ZE-Cat Module
+** Copyright 2014 - Adail Horst - http://spinola.net.br/blog
+**
+** This file is part of Zabbix-Extras.
+** It is not authorized any change that would mask the existence of the plugin. 
+** The menu names, logos, authorship and other items identificatory plugin 
+** should always be maintained.
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+** If not, see http://www.gnu.org/licenses/.
+**/
+?>
 <!DOCTYPE html>
 
 <html>
