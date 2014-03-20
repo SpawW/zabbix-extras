a85a46cd0e9c4e23fc9366d4164973c0242c07f1
spaww <the.spaww@gmail.com>
2014-03-20 09:32:47 -0300

    2.0.1 - Custom Logo for frontend
    Dinamic Logo from Zabbix Image catalog

--- zbxe-logo.php
+++ zbxe-logo.php
@@ -1,11 +1,29 @@
 <?php
 
-/* 
- * Objetivo: Gerar dinamicamente o logotipo do zabbix permitindo customizacao por usuario
- * Adail Horst - http://spinola.net.br/blog
- * Parte integrante do zabbix-extras
- * Nao e permitida qualquer alteracao que renomeie ou complique a identificacao do produto
- */
+/* Used for inicial development: 
+** Objective: Show a logo for use in frontend logo based on image from Zabbix 
+** network map images
+** Copyright 2014 - Adail Horst - http://spinola.net.br/blog
+**
+** This file is part of Zabbix-Extras.
+** It is not authorized any change that would mask the existence of the plugin. 
+** The menu names, logos, authorship and other items identificatory plugin 
+** should always be maintained.
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**/
 
 require_once dirname(__FILE__).'/include/config.inc.php';
 require_once dirname(__FILE__).'/include/zbxe_visual_imp.php';
